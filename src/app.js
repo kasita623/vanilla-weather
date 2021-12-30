a6731f8010a3444ef0a014bf852e128e8ccdd612
kasita623 <kasiajura@onet.pl>
2021-12-30 12:50:16 +0100

    Forecast HTML, CSS and JS

--- src/app.js
+++ src/app.js
@@ -110,4 +110,33 @@ celciusElement.addEventListener("click", displayCelcius);
 
 let celciusValue = null;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+           <div class="col-2">
+             <div class="weather-forecast-day">
+               ${day}
+             </div> 
+             <img src="http://openweathermap.org/img/wn/04d@2x.png" width="40"/>
+             <div class="weather-forecast-temperature">
+               <span class="weather-forecast-temperature-max">
+                 14°
+               </span>
+               <span class="weather-forecast-temperature-min">
+                 12°
+               </span>
+             </div>
+           </div> `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+displayForecast();
+
 search("London");
