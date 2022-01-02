e2750c40c9ebf4d190f3393a9b7c34ed36ffa337
kasita623 <kasiajura@onet.pl>
2022-01-02 11:43:12 +0100

    Creasted forecast API in JS

--- src/app.js
+++ src/app.js
@@ -28,6 +28,12 @@ function formatDate(timestamp) {
   return `${day} ${date}/${month}`;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "940cab7f2dffe0039b455473a663a1f7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeather(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -70,6 +76,7 @@ function displayWeather(response) {
   } else {
     reminder.innerHTML = `Have an amazing day!`;
   }
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -110,7 +117,8 @@ celciusElement.addEventListener("click", displayCelcius);
 
 let celciusValue = null;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -137,6 +145,5 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-displayForecast();
 
 search("London");
